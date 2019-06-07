dd2e6ec647fe6359b0ac2d9afeb52fa1db432819
vitebo <andre.vitebo@redealumni.com>
2019-06-07 17:43:55 -0300

    add sass

--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,16 @@
         My wondrous Nuxt.js project
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
-          >GitHub</a
         >
+          GitHub
+        </a>
       </div>
     </div>
   </div>
@@ -33,7 +34,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
