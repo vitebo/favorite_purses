5fdad7fa4d1f6c071190135232e74c4c90913395
vitebo <andre.vitebo@redealumni.com>
2019-06-08 19:18:25 -0300

    rename base router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,8 @@
-/* nuxt.config.js */
-// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: 'https://vitebo.github.io/favorite_purses'
+          base: '/favorite_purses/'
         }
       }
     : {}
