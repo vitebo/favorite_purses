258e88f91962e069b379f0c1578962ec257c9287
vitebo <andre.vitebo@redealumni.com>
2019-06-07 23:57:45 -0300

    create header

---
+++ components/QHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="q-header">
+    <div class="q-header__item q-header__item--first">
+      Ajuda
+    </div>
+    <div class="q-header__item">
+      Logo
+    </div>
+    <div class="q-header__item q-header__item--last">
+      Login
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+$header-height: 64px;
+$header-border-color: #ebebeb;
+$divisor-color: #eeeeee;
+
+.q-header {
+  align-items: center;
+  background-color: $neutral-color-white;
+  border-bottom: solid 1px $header-border-color;
+  box-shadow: $low-box-shadow;
+  display: flex;
+  height: $header-height;
+  justify-content: space-between;
+}
+
+.q-header__item {
+  padding: $space-s;
+}
+
+.q-header__item--first {
+  border-right: $space-xxs solid $divisor-color;
+}
+
+.q-header__item--last {
+  border-left: $space-xxs solid $divisor-color;
+}
+</style>
