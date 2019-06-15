42792d9eab3d9c981859ee5085d5cbc74085ab97
vitebo <andre.vitebo@redealumni.com>
2019-06-15 15:15:06 -0300

    create copyright

--- components/layout/footer/QFooter.vue
+++ components/layout/footer/QFooter.vue
@@ -1,43 +1,53 @@
 <template>
   <footer class="q-footer">
-    <ul class="q-footer__list">
-      <li class="q-footer__item">
-        <q-footer-item
-          title="0800 123 2222"
-          description="Seg - Sex 8h-22h"
-          short-description="Segunda a sexta de 8h às 22h"
-        >
-          <q-icon-whats-app />
-        </q-footer-item>
-      </li>
-      <li class="q-footer__item">
-        <q-footer-item
-          title="Chat ao vivo"
-          description="Seg - Sex 8h-22h"
-          short-title="Chat"
-        >
-          <q-icon-comments />
-        </q-footer-item>
-      </li>
-      <li class="q-footer__item">
-        <q-footer-item
-          title="Mande um e-mail"
-          description="Respondemos rapidinho"
-          short-title="E-mail"
-        >
-          <q-icon-envelope />
-        </q-footer-item>
-      </li>
-      <li class="q-footer__item">
-        <q-footer-item
-          title="Central de ajuda"
-          description="Encontre todas as respostas"
-          short-title="Ajuda"
-        >
-          <q-icon-info />
-        </q-footer-item>
-      </li>
-    </ul>
+    <div class="q-footer__container">
+      <ul class="q-footer__list">
+        <li class="q-footer__item q-footer__item--large">
+          <q-footer-item
+            title="0800 123 2222"
+            description="Seg - Sex 8h-22h"
+            short-description="Segunda a sexta de 8h às 22h"
+            modifier="large"
+          >
+            <q-icon-whats-app />
+          </q-footer-item>
+        </li>
+        <li class="q-footer__item q-footer__item--small">
+          <q-footer-item
+            title="Chat ao vivo"
+            description="Seg - Sex 8h-22h"
+            short-title="Chat"
+          >
+            <q-icon-comments />
+          </q-footer-item>
+        </li>
+        <li class="q-footer__item q-footer__item--small">
+          <q-footer-item
+            title="Mande um e-mail"
+            description="Respondemos rapidinho"
+            short-title="E-mail"
+          >
+            <q-icon-envelope />
+          </q-footer-item>
+        </li>
+        <li class="q-footer__item q-footer__item--small">
+          <q-footer-item
+            title="Central de ajuda"
+            description="Encontre todas as respostas"
+            short-title="Ajuda"
+          >
+            <q-icon-info />
+          </q-footer-item>
+        </li>
+      </ul>
+      <section class="q-footer__copyright">
+        Feito com
+        <q-icon-base height="16px" width="26px">
+          <q-icon-heart :animated="true" />
+        </q-icon-base>
+        pela Quero Educação
+      </section>
+    </div>
   </footer>
 </template>
 
@@ -48,6 +58,8 @@ import QIconInfo from '~/components/q-icons/QIconInfo'
 import QIconWhatsApp from '~/components/q-icons/QIconWhatsApp'
 import QIconComments from '~/components/q-icons/QIconComments'
 import QIconEnvelope from '~/components/q-icons/QIconEnvelope'
+import QIconHeart from '~/components/q-icons/QIconHeart'
+import QIconBase from '~/components/q-icons/QIconBase'
 
 export default {
   components: {
@@ -55,23 +67,54 @@ export default {
     QIconWhatsApp,
     QIconComments,
     QIconEnvelope,
-    QIconInfo
+    QIconInfo,
+    QIconBase,
+    QIconHeart
   }
 }
 </script>
 
 <style lang="scss">
 .q-footer {
-  background-color: $primary-color-dark;
+  background-color: $primary-color;
+}
+
+.q-footer__container {
+  @extend %container;
 }
 
 .q-footer__list {
-  @extend %container;
+  @extend %row;
   display: flex;
+  flex-wrap: wrap;
   list-style: none;
+  padding-left: 0;
+  margin-top: 0;
+  margin-bottom: 0;
 }
 
 .q-footer__item {
-  flex: 1;
+  flex: 1 1 auto;
+}
+
+.q-footer__item--large {
+  flex-basis: 100%;
+}
+
+.q-footer__item--small {
+  flex-basis: 0;
+  margin-right: $space-xxs;
+  margin-top: $space-xxs;
+
+  &:last-child {
+    margin-right: 0;
+  }
+}
+
+.q-footer__copyright {
+  padding: $space-st 0;
+  text-align: center;
+  color: $neutral-color-white;
+  font-size: $font-size-small;
 }
 </style>
--- components/layout/footer/QFooterItem.vue
+++ components/layout/footer/QFooterItem.vue
@@ -1,15 +1,15 @@
 <template>
-  <a href="#" class="q-footer-item">
-    <q-icon-base>
+  <a href="#" :class="qFooterItemClass">
+    <q-icon-base class="q-footer-item__icon" height="32px" width="32px">
       <slot />
     </q-icon-base>
-    <div>
-      <strong>{{ title }}</strong>
-      <p>{{ descrition }}</p>
+    <div class="q-footer-item__content q-footer-item__content--desktop">
+      <strong class="q-footer-item__title">{{ title }}</strong>
+      <p class="q-footer-item__description">{{ description }}</p>
     </div>
-    <div>
-      <strong>{{ getShortTitle }}</strong>
-      <p>{{ getShortDescription }}</p>
+    <div class="q-footer-item__content q-footer-item__content--mobile">
+      <strong class="q-footer-item__title">{{ getShortTitle }}</strong>
+      <p class="q-footer-item__description">{{ getShortDescription }}</p>
     </div>
   </a>
 </template>
@@ -37,6 +37,10 @@ export default {
     shortDescription: {
       type: [String, Boolean],
       default: false
+    },
+    modifier: {
+      type: [String, Boolean],
+      default: false
     }
   },
   computed: {
@@ -45,6 +49,14 @@ export default {
     },
     getShortDescription() {
       return this.shortDescription || this.description
+    },
+    qFooterItemClass() {
+      const className = 'q-footer-item'
+
+      const modifier = {}
+      modifier[`${className}--${this.modifier}`] = this.modifier
+
+      return [className, modifier]
     }
   }
 }
@@ -52,7 +64,58 @@ export default {
 
 <style lang="scss">
 .q-footer-item {
+  background-color: $primary-color-dark;
   color: $neutral-color-white;
+  display: flex;
+  flex-direction: column;
+  padding: $space-m $space-st;
   text-decoration: none;
+  text-align: center;
+}
+
+.q-footer-item--large {
+  align-items: center;
+  flex-direction: row;
+  text-align: left;
+
+  .q-footer-item__content--mobile {
+    & > .q-footer-item__description {
+      display: block;
+    }
+  }
+
+  .q-footer-item__icon {
+    margin-right: $space-s;
+    margin-left: 0;
+  }
+}
+
+.q-footer-item__icon {
+  @include min-size(32px);
+  margin: 0 auto;
+}
+
+.q-footer-item__content--desktop {
+  @include media-breakpoint-down(sm) {
+    display: none;
+  }
+}
+
+.q-footer-item__content--mobile {
+  @include media-breakpoint-up(sm) {
+    display: none;
+  }
+
+  & > .q-footer-item__description {
+    display: none;
+  }
+}
+
+.q-footer-item__title {
+  margin: 0;
+}
+
+.q-footer-item__description {
+  margin: 0;
 }
 </style>
