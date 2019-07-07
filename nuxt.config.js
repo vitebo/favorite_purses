48c0a8ee3cd60a78b8289e15aab4f8c47c804cef
vitebo <andre.vitebo@redealumni.com>
2019-07-07 16:32:11 -0300

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Quero Bolsa',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
